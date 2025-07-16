600113585f4ccc7987e6208560cdd6dd2feb72f4
Hrishikesh <hrishigaonkar2@gmail.com>
2025-07-16 20:15:17 +0530

    urlsetup

--- src/landing_page/signup/Login.js
+++ src/landing_page/signup/Login.js
@@ -19,7 +19,7 @@ const Login = () => {
       const res =await axios.post(`${process.env.REACT_APP_BACKEND_URL}/login`, formData,{ withCredentials: true}).then(()=>{
       toast.success('Login successful ✅');
       setTimeout(() => {
-        window.location.href = 'https://zerodha-dashboard-brown.vercel.app'; // ✅ Redirect to dashboard
+        window.location.href = 'www.google.com'; // ✅ Redirect to dashboard
       }, 2000);
       }).catch((err)=>{
         toast.error(err.response?.data?.msg || 'Invalid username or password ❌');
